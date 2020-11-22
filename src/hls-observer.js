41df0784193cce503006dda8d92097075c0a64d8
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-11-22 03:43:47 -0300

    chore: fix customized output example

--- src/hls-observer.js
+++ src/hls-observer.js
@@ -9,12 +9,19 @@ export default class HLSObserver extends Transcriptore {
   readManifest(buffer) {
     const manifest = this.readBuffer(buffer)
     const linesArray = manifest.split('\n')
-    const customizedLinesArray = manifest.replace(/.m3u8\n/g, '.m3u8 - ').split('\n')
     const itemsArray = manifest.replace(/\n/g, ',').split(',')
 
     console.log('>>>>>> manifest:', `\n\n${manifest}\n\n`) // eslint-disable-line no-console
     console.log('>>>>>> array of manifest lines:', linesArray) // eslint-disable-line no-console
     console.log('>>>>>> array of manifest items:', itemsArray) // eslint-disable-line no-console
-    customizedLinesArray.forEach((line, iterator) => console.log(`Line ${++iterator}: ${line}`)) // eslint-disable-line
+
+    const customizedLinesArray = [...linesArray]
+    customizedLinesArray.forEach((item, iterator) => {
+      if (item.match(/.m3u8/g)) {
+        customizedLinesArray[iterator - 1] = `${item} - ${customizedLinesArray[iterator - 1]}`
+        customizedLinesArray.splice(iterator, 1)
+      }
+    })
+    customizedLinesArray.forEach((line, iterator) => console.log(`Line ${++iterator} - ${line}`)) // eslint-disable-line
   }
 }
